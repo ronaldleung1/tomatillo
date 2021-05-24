b52082be62f15913b17b7d55fb0b0cb922a5dfac
googol88 <ronaldfleung@gmail.com>
2021-05-24 11:00:17 -0400

    🎨 timer state

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,18 @@
-import { Text } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Button
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 
-export const Timer = () => (
-  <Text>This is a timer. Very impressive</Text>
-)
+export const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <>
+      <Box border="1px" borderColor="gray.200" borderRadius={4} p={4}>
+        <Text>This is a timer. Very impressive</Text>
+      </Box>
+    </>
+  )
+}
