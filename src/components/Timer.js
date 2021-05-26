d77f6bd86ad880be3410c3d3507ce74c45224f04
googol88 <ronaldfleung@gmail.com>
2021-05-26 10:32:37 -0400

    ✨ add state hooks for start/stop and reset

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,17 +7,27 @@ import {
 import React, { useState } from 'react';
 
 export const Timer = () => {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(20);
   const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
   return (
     <>
       <Box border="1px" minW="400px" borderColor="gray" borderRadius={4} p={8}>
-        <Text fontSize="6xl" textAlign="center">00:00</Text>
+        <Text fontSize="6xl" textAlign="center">{seconds}s</Text>
         <Stack spacing={4} mt={4} direction="row" justifyContent="center" align="center">
-          <Button colorScheme="teal">
+          <Button colorScheme="blue" minWidth={24} onClick={toggle}>
             {isActive ? 'Pause' : 'Start'}
           </Button>
-          <Button colorScheme="gray">
+          <Button colorScheme="gray" onClick={reset}>
             Reset
           </Button>
         </Stack>
