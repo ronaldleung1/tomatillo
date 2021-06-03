f7a292cccd495b310fd881287b2dd5a61a545fab
Googol <57511576+googol88@users.noreply.github.com>
2021-06-03 16:16:54 +0000

    🎨 create event handler for task list input

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,20 @@
 import { Input, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from 'react';
 
-export const TaskList = (isActive, props) => {
+export const TaskList = (props) => {
   const [tasks, setTasks] = useState([
     "Task 1",
     "Task 2",
     "Task 3",
   ]);
+
+  const handleSubmit = (e) => {
+    setTasks([e.target.value]);
+  }
+
   return (
     <>
-      <Input placeholder="Task" {...props} />
+      <Input onSubmit={handleSubmit} placeholder="Task" {...props}/>
       {tasks.map((task) =>
         <Text>{task}</Text>
       )}
