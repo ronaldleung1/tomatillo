fd1f9e458999dfb4a8be750a2448adc74fb838bf
googol88 <ronaldfleung@gmail.com>
2021-06-03 11:04:59 -0400

    ✨ create tasks and map

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,18 @@
-import { Input } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from 'react';
 
-export const TaskList = (props) => {
+export const TaskList = (isActive, props) => {
+  const [tasks, setTasks] = useState([
+    "Task 1",
+    "Task 2",
+    "Task 3",
+  ]);
   return (
-    <Input placeholder="Task" {...props} />
+    <>
+      <Input placeholder="Task" {...props} />
+      {tasks.map((task) =>
+        <Text>{task}</Text>
+      )}
+    </>
   )
 }
