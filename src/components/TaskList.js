3a39bf2fad6ecab118fae1b6aebe3d9b4104a5be
googol88 <ronaldfleung@gmail.com>
2021-06-03 20:00:27 -0400

    🐛 fix form input

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,20 +2,32 @@ import { Input, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from 'react';
 
 export const TaskList = (props) => {
+  // array containing names of tasks
   const [tasks, setTasks] = useState([
     "Task 1",
     "Task 2",
     "Task 3",
   ]);
+  // current value in form input
+  const [value, setValue] = useState("");
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  } 
   const handleSubmit = (e) => {
-    setTasks([e.target.value]);
+    // spread operator to push new value to tasks array
+    setTasks(values => [...values, value]);
+    // clears form input
+    setValue("");
+    e.preventDefault();
   }
 
   return (
     <>
-      <Input onSubmit={handleSubmit} placeholder="Task" {...props}/>
-      {tasks.map((task) =>
+      <form onSubmit={handleSubmit}>
+        <Input placeholder="Task" value={value} onChange={handleChange} {...props}/>
+      </form>
+      {tasks.map((task) => // puts each task in its own Text component
         <Text>{task}</Text>
       )}
     </>
