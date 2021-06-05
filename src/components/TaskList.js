1cf50493d3c4d9e83db3020a4d9d27c75799f1f1
googol88 <ronaldfleung@gmail.com>
2021-06-05 09:56:46 -0400

    ✨ Delete tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
-import { Input, Text } from "@chakra-ui/react";
+import { Input, Flex, Button, Text, Spacer } from "@chakra-ui/react";
 import React, { useEffect, useState } from 'react';
+import { Trash } from "react-feather";
 
 export const TaskList = (props) => {
   // array containing names of tasks
@@ -18,14 +19,29 @@ export const TaskList = (props) => {
     e.preventDefault();
   }
 
+  const handleDelete = (index) => {
+    // filters out tasks with matching index
+    const newTasks = tasks.filter((task, id) => id !== index);
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <Input placeholder="Task" value={value} onChange={handleChange} {...props}/>
       </form>
-      {tasks.map((task, index) => // puts each task in its own Text component
-        <Text key={index}>{task}</Text>
-      )}
+      <Flex direction="column-reverse">
+        {tasks.map((task, index) => // puts each task in its own Text component
+          <Flex my={2} py={3} px={6} alignItems="center" boxShadow="base" textAlign="left" borderWidth="1px" borderRadius="lg">
+            <Text key={index}>{task}</Text>
+            <Spacer />
+            {/* arrow function `=>` so handleDelete isn't called on render */}
+            <Button onClick={() => handleDelete(index)} colorScheme="red" variant="ghost">
+              <Trash size={18}/>
+            </Button>
+          </Flex>
+        )}
+      </Flex>
     </>
   )
 }
