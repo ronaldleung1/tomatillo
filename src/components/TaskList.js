876e964347f0aa2cf4bdf0467c55a1663f3ec9e3
googol88 <ronaldfleung@gmail.com>
2021-06-07 10:08:04 -0400

    ✨ Fetch from GitHub API

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
-import { Input, Flex, Button, Text, Spacer } from "@chakra-ui/react";
+import { Input, Flex, Box, Button, Text, Spacer } from "@chakra-ui/react";
 import React, { useEffect, useState } from 'react';
+// import useSWR from 'swr'; // fetching from GitHub API
 import { Trash } from "react-feather";
 
 export const TaskList = (props) => {
@@ -7,13 +8,21 @@ export const TaskList = (props) => {
   const [tasks, setTasks] = useState([]);
   // current value in form input
   const [value, setValue] = useState("");
+  const [repo, setRepo] = useState(null);
 
+  // wrapper of fetch() parsed to JSON
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  
   const handleChange = (e) => {
     setValue(e.target.value);
   } 
+
   const handleSubmit = (e) => {
+    fetcher(`https://api.github.com/repos/${value}`)
+      .then(repo => setRepo(repo))
+      .then(repo => setTasks(values => [...values, repo]))
     // spread operator to push new value to tasks array
-    setTasks(values => [...values, value]);
+    
     // clears form input
     setValue("");
     e.preventDefault();
@@ -32,8 +41,11 @@ export const TaskList = (props) => {
       </form>
       <Flex direction="column-reverse">
         {tasks.map((task, index) => // puts each task in its own Text component
-          <Flex my={2} py={3} px={6} alignItems="center" boxShadow="base" textAlign="left" borderWidth="1px" borderRadius="lg">
-            <Text key={index}>{task}</Text>
+          <Flex key={index} my={2} py={3} px={6} alignItems="center" boxShadow="base" textAlign="left" borderWidth="1px" borderRadius="lg">
+            <Box>
+              <Text>{repo["full_name"]}</Text>
+              <Text>{repo["description"]}</Text>
+            </Box>
             <Spacer />
             {/* arrow function `=>` so handleDelete isn't called on render */}
             <Button onClick={() => handleDelete(index)} colorScheme="red" variant="ghost">
